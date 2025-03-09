1edd368a22f554d43badcaecd722b14ab49f56fb
BhashiBandara95 <ugabkanishka@gmail.com>
2025-03-09 14:03:12 +0530

    add portfolio

---
+++ portfolio/app/layout.js
@@ -0,0 +1,27 @@
+import { Outfit, Ovo } from "next/font/google";
+import "./globals.css";
+
+const Outfit = Outfit({
+	subsets: ["latin"],
+	weight: ["400", "500", "600", "700"],
+});
+
+const Ovo = Ovo({
+	subsets: ["latin"],
+	weight: ["400"],
+});
+
+export const metadata = {
+	title: "Portfolio - Bhashitha Kanishka",
+	description: "",
+};
+
+export default function RootLayout({ children }) {
+	return (
+		<html lang="en">
+			<body className={`${Outfit.className} ${Ovo.className} antialiased`}>
+				{children}
+			</body>
+		</html>
+	);
+}
---
+++ portfolio/app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+	return (
+		<div>
+			<h3>This is H3 Tag</h3>
+		</div>
+	);
+}
